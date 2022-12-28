35e8fa895e220e6297e55ee66d99865d3c44312e
sahathaisteelpipe <sahathaisteelpipe.itsup@gmail.com>
2022-12-28 15:48:34 +0700

    Qc_TestLab

---
+++ models/_SyteLine/QcTestLab.php
@@ -0,0 +1,54 @@
+<?php
+class QcTestLab {
+    var $StrConn = "";
+
+    function setConn($c) {
+        $this->StrConn = $c;
+    }
+
+    function SearchQcTestLab_Main($where){
+        $query = "select * from STS_QA_LAB ".$where."";
+        $cSql = new SqlSrv();
+        $rs0 = $cSql->SqlQuery($this->StrConn, $query);
+        array_splice($rs0, count($rs0) - 1, 1);
+        return $rs0;
+    }
+
+    function SearchQcTestLab_Sub($where){
+        $query = "select * from STS_QA_LAB_SUB ".$where."";
+        $cSql = new SqlSrv();
+        $rs0 = $cSql->SqlQuery($this->StrConn, $query);
+        array_splice($rs0, count($rs0) - 1, 1);
+        return $rs0;
+    }
+
+    function InsertQcTestLab_Main($standard,$thick,$width,$weight,$sts_no,$c_no,$h_no,
+    $sup_c,$sup_si,$sup_mn,$sup_p,$sup_s,$sup_cu,$sup_v,$sup_ni,$sup_cr,$sup_mo,$sup_ti,$sup_nb,$sup_al,$sup_b,$sup_co,$sup_pb,$sup_fe,$sup_ts,$sup_ys,$sup_el,
+    $sts_c,$sts_si,$sts_mn,$sts_p,$sts_s,$sts_cu,$sts_v,$sts_ni,$sts_cr,$sts_mo,$sts_ti,$sts_nb,$sts_al,$sts_b,$sts_co,$sts_pb,$sts_fe,$sts_ts,$sts_ys,$sts_el) {
+
+         $query ="INSERT INTO STS_QA_LAB (standard,thick,width,weight,sts_no,c_no,h_no,"
+                ."sup_c,sup_si,sup_mn,sup_p,sup_s,sup_cu,sup_v,sup_ni,sup_cr,sup_mo,sup_ti,sup_nb,sup_al,sup_b,sup_co,sup_pb,sup_fe,sup_ts,sup_ys,sup_el,"
+                ."sts_c,sts_si,sts_mn,sts_p,sts_s,sts_cu,sts_v,sts_ni,sts_cr,sts_mo,sts_ti,sts_nb,sts_al,sts_b,sts_co,sts_pb,sts_fe,sts_ts,sts_ys,sts_el,ImportDate)"
+                ."VALUES ('".$standard."', '".$thick."', '".$width."', ".$weight.", '".$sts_no."', '".$c_no."', '".$h_no."',"
+                ."'".$sup_c."', '".$sup_si."', '".$sup_mn."', '".$sup_p."', '".$sup_s."', '".$sup_cu."', '".$sup_v."', '".$sup_ni."', '".$sup_cr."', '".$sup_mo."', '".$sup_ti."', '".$sup_nb."', '".$sup_al."', '".$sup_b."', '".$sup_co."', '".$sup_pb."', '".$sup_fe."', '".$sup_ts."', '".$sup_ys."', '".$sup_el."',"
+                ."'".$sts_c."', '".$sts_si."', '".$sts_mn."', '".$sts_p."', '".$sts_s."', '".$sts_cu."', '".$sts_v."', '".$sts_ni."', '".$sts_cr."', '".$sts_mo."', '".$sts_ti."', '".$sts_nb."', '".$sts_al."', '".$sts_b."', '".$sts_co."', '".$sts_pb."', '".$sts_fe."', '".$sts_ts."', '".$sts_ys."', '".$sts_el."',Getdate())";
+        $cSql = new SqlSrv();
+        $rs0 = $cSql->SqlQuery($this->StrConn, $query);
+        array_splice($rs0, count($rs0) - 1, 1);
+        return $rs0; 
+    }
+
+    function InsertQcTestLab_Sub($size,$thick,$length,$standard,$sts_no,$Mec_test_TS,$Mec_test_YS,$Mec_test_EI,
+    $Mec_test_EL_1,$Mec_test_EL_2,$Mec_test_EL_3,$charpy_mean,$charpy1,$charpy2,$charpy3,$Metal_P,$Metal_F,$Metal_M,$Hydro_test,$prod_FM_no,$prod_date,$test_date) {
+
+            $query ="INSERT INTO STS_QA_LAB_SUB (size,thick_sub,length,standard_sub,sts_no,Mec_test_TS,Mec_test_YS,Mec_test_EI,"
+                    ."Mec_test_EL_1,Mec_test_EL_2,Mec_test_EL_3,charpy_mean,charpy1,charpy2,charpy3,Metal_P,Metal_F,Metal_M,Hydro_test,prod_FM_no,prod_date,test_date,ImportDate)"
+                    ."VALUES ('".$size."', '".$thick."', '".$length."', '".$standard."', '".$sts_no."', '".$Mec_test_TS."', '".$Mec_test_YS."','".$Mec_test_EI."',"
+                    ."'".$Mec_test_EL_1."', '".$Mec_test_EL_2."', '".$Mec_test_EL_3."', '".$charpy_mean."', '".$charpy1."', '".$charpy2."', '".$charpy3."', '".$Metal_P."', '".$Metal_F."', '".$Metal_M."', '".$Hydro_test."', '".$prod_FM_no."', '".$prod_date."', '".$test_date."',Getdate())";
+            $cSql = new SqlSrv();
+            $rs0 = $cSql->SqlQuery($this->StrConn, $query);
+            array_splice($rs0, count($rs0) - 1, 1);
+            return $rs0; 
+    }
+}
+?>
---
+++ module/APP_Qc_TestLab/upexcel_sub.php
@@ -0,0 +1,11 @@
+<?php
+
+while (list($key, $data) = each($_GET) OR list($key, $data) = each($_POST)) {
+    ${$key} = trim($data);
+}
+
+include "../initial.php";
+
+$temp = new ReplaceHtml("../../template/APP_Qc_TestLab/upexcel_sub.html");
+echo $temp->getReplace();
+sqlsrv_close($ConnSL);
