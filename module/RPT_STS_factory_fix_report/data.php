0153270c8344789235eb60aaf0239c8c86ce0edb
peeraphut punsuwan <>
2024-04-28 15:30:50 +0700

    RPT_STS_factory_fix_report 2

--- module/RPT_STS_factory_fix_report/data.php
+++ module/RPT_STS_factory_fix_report/data.php
@@ -11,6 +11,13 @@ $CallModelObj = new Factory();
 $CallModelObj->setConn($ConnSL);
 
 if ($load == "ajax") {
+    (isset($TransactionDateStarting)) ? $TransactionDateStarting = $TransactionDateStarting : $TransactionDateStarting = "";
+    (isset($TransactionDateEnding)) ? $TransactionDateEnding = $TransactionDateEnding : $TransactionDateEnding = "";
+	(isset($unit1)) ? $unit1 = $unit1 : $unit1 = 0;
+
+    $RPT_STS_factory_fix_report = $CallModelObj->RPT_STS_factory_fix_report($TransactionDateStarting,$TransactionDateEnding,$unit1);
+    echo json_encode($RPT_STS_factory_fix_report);
+
 } else if ($load == "RPT_STS_factory_fix_report") {
     (isset($TransactionDateStarting)) ? $TransactionDateStarting = $TransactionDateStarting : $TransactionDateStarting = "";
     (isset($TransactionDateEnding)) ? $TransactionDateEnding = $TransactionDateEnding : $TransactionDateEnding = "";
