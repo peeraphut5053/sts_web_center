66eeff7cffb09c2513fac188bfc3f637a9e1e7f7
Pongteedee <pongteedee@gmail.com>
2024-05-28 13:05:09 +0700

    Add Report General Ledger Domestic Invoice

--- models/_SyteLine/Factory.php
+++ models/_SyteLine/Factory.php
@@ -3,6 +3,8 @@
 class Factory {
 
     var $StrConn = "";
+    public $_StartDate = "";
+    public $_EndDate = "";
 
     function setConn($c) {
         $this->StrConn = $c;
@@ -31,6 +33,30 @@ class Factory {
 
         $query = "select distinct led.acct_unit1, unit1.[description] as unit1_description from ledger_mst led left join unitcd1_mst unit1 on led.acct_unit1=unit1.unit1 where led.ref like 'AP%' and led.acct like '5315%' ";
         $rs0 = $cSql->SqlQuery($this->StrConn, $query);
+        array_splice($rs0, count($rs0) - 1, 1);
+        return $rs0;
+    }
+
+    Function RPT_General_ledger_report() {
+        $StartDate = $this->_StartDate." 00:00:00" ;
+        $EndDate = $this->_EndDate." 23:59:59" ;
+        $cSql = new SqlSrv();
+        $q = "SELECT 
+        led.acct,
+        chart.[description] AS acct_name,
+        CONVERT(DATE, led.trans_date) AS trans_date,
+        led.dom_amount,
+        led.ref,
+        led.vend_num,
+        led.voucher
+    FROM 
+        ledger_mst led
+    INNER JOIN 
+        chart_mst chart ON chart.acct = led.acct
+    WHERE 
+        (led.ref LIKE '%IN%' OR led.ref LIKE '%CN%' OR led.ref LIKE '%DN%')
+        AND CONVERT(DATE, led.trans_date) BETWEEN '$StartDate' AND '$EndDate'";
+        $rs0 = $cSql->SqlQuery($this->StrConn, $q);
         array_splice($rs0, count($rs0) - 1, 1);
         return $rs0;
     }
---
+++ module/RPT_GENERAL_LEDGER_DOMESTIC_INVOICE/data.php
@@ -0,0 +1,21 @@
+<?php
+
+while (list($key, $data) = each($_GET) OR list($key, $data) = each($_POST)) {
+    ${$key} = trim($data);
+}
+include "../../initial.php";
+
+if ($load == "form") {
+    
+} else if ($load == "ajax") {
+    $CM = new CallModel();
+    $CM->SyteLine_Models();
+    $AT = new Factory();
+    $AT->setConn($ConnSL);
+    $AT->_StartDate = $StartDate;
+    $AT->_EndDate = $EndDate;
+    $General = $AT->RPT_General_ledger_report();
+    echo json_encode($General);
+} 
+
+
