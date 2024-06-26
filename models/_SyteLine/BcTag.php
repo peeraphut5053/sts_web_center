956be1f064d9368c0821c5f391a7be1342c46a3f
Pongteedee <pongteedee@gmail.com>
2024-06-26 14:25:49 +0700

    Fix RPT_QC_Lab_Tag_Detail

--- models/_SyteLine/BcTag.php
+++ models/_SyteLine/BcTag.php
@@ -502,18 +502,19 @@ where STS_qty_move_line.doc_num = '$doc_num' and mv_bc_tag.active=1 and mv_bc_ta
         } else {
             $wh = "";
         }
-        
+    
         $query = "select  qc_oper_num , id 
   , uf_nps as size, item_mst.uf_schedule, item_mst.uf_length, item_mst.uf_spec as [standard]
   , item_mst.uf_grade,mv_bc_tag.sts_no
   , sts_po_qc.c_no as Coil_No, STS_po_qc.h_no as Heat_no
   , matltran_mst.wc as FM, concat(sts_po_qc.thick,' x ',sts_po_qc.width) as thick
-  , convert(date,mv_bc_tag.mfg_date) as [date]
+  , convert(date,mv_bc_tag.mfg_date) as [date], mv_bc_tag.item
 from mv_bc_tag inner join item_mst on item_mst.item = mv_bc_tag.item
       inner join sts_po_qc on sts_po_qc.sno = mv_bc_tag.sts_no 
       inner join matltran_mst on matltran_mst.lot = mv_bc_tag.lot 
       and matltran_mst.trans_type='F'
-where mv_bc_tag.active = 1 $wh $date ";
+where mv_bc_tag.active = 1 and matltran_mst.wc like '%FM%' $wh $date ";
+       
         $cSql = new SqlSrv();
         $rs0 = $cSql->SqlQuery($this->StrConn, $query);
         array_splice($rs0, count($rs0) - 1, 1);
