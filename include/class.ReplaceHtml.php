72ac5e2815c7bfca93f7f3b83c2e89713df1c15e
Pongteedee <pongteedee@gmail.com>
2024-09-13 09:23:05 +0700

    Fix

--- include/class.ReplaceHtml.php
+++ include/class.ReplaceHtml.php
@@ -1,23 +1,21 @@
 <?php
-class ReplaceHtml {
-    
-    private $mainDetail;
+class ReplaceHtml{
+	var $mainDetail;
 
-    // เปลี่ยนจาก function ReplaceHtml($filePath) เป็น __construct($filePath)
-    public function __construct($filePath) {
-        $this->mainDetail = $this->getTemplate($filePath);
-    }
+	function ReplaceHtml($filePath){
+		$this->mainDetail = $this->getTemplate($filePath);
+	}
 
-    public function setReplace($sign, $data) {
-        $this->mainDetail = str_replace($sign, $data, $this->mainDetail);
-    }    
+	function setReplace($sign,$data){
+		$this->mainDetail = str_replace($sign,$data,$this->mainDetail);
+	}	
 
-    public function getReplace() {
-        return $this->mainDetail;
-    }
+	function getReplace(){
+		return $this->mainDetail;
+	}
 
-    public function getTemplate($filePath = "") {
-        return implode('', file($filePath));                    
-    } 
+	function getTemplate($filePath="") {
+		return implode('',file($filePath));					
+	} 
 }
 ?>
