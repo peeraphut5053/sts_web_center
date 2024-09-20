e9bc93bc7e98ad5fc47c945edd86e54dd65d1c18
Pongteedee <pongteedee@gmail.com>
2024-09-20 10:26:54 +0700

    Fix Sql Query HRC In Process of FM For Lab

--- models/_SyteLine/BcTag.php
+++ models/_SyteLine/BcTag.php
@@ -505,7 +505,8 @@ where STS_qty_move_line.doc_num = '$doc_num' and mv_bc_tag.active=1 and mv_bc_ta
     
         $query = "select  qc_oper_num , id 
   , uf_nps as size, item_mst.uf_schedule, item_mst.uf_length, item_mst.uf_spec as [standard]
-  , item_mst.uf_grade,mv_bc_tag.sts_no
+  , item_mst.uf_grade
+   ,sts_no = sts_po_qc.sno --mv_bc_tag.sts_no
   , STS_QA_LAB.c_no as Coil_No, STS_po_qc.h_no as Heat_no
   , matltran_mst.wc as FM, concat(sts_po_qc.thick,' x ',sts_po_qc.width) as thick
   , convert(date,mv_bc_tag.mfg_date) as [date], mv_bc_tag.item
@@ -532,16 +533,20 @@ where STS_qty_move_line.doc_num = '$doc_num' and mv_bc_tag.active=1 and mv_bc_ta
   , STS_QA_LAB.sts_ys
   , STS_QA_LAB.sts_el
 from mv_bc_tag inner join item_mst on item_mst.item = mv_bc_tag.item
-      inner join sts_po_qc on sts_po_qc.sno = mv_bc_tag.sts_no 
-      inner join matltran_mst on matltran_mst.lot = mv_bc_tag.lot 
+      inner join sts_po_qc on sno <> '' and
+        ( Ltrim(rtrim(sts_po_qc.sno)) = mv_bc_tag.sts_no 
+        or Ltrim(rtrim(sts_po_qc.sno)) = Ltrim(rtrim(mv_bc_tag.sts_no2))
+        or Ltrim(rtrim(sts_po_qc.sno)) = Ltrim(rtrim(mv_bc_tag.sts_no3))
+         )
+
+   inner join matltran_mst on matltran_mst.lot = mv_bc_tag.lot --and matltran_mst.item = mv_bc_tag.item
       and matltran_mst.trans_type='F'
-   left join STS_QA_LAB on STS_QA_LAB.sts_no = sts_po_qc.sno 
+   left join STS_QA_LAB on Ltrim(rtrim(STS_QA_LAB.sts_no)) = Ltrim(rtrim(sts_po_qc.sno)) 
  --  and STS_QA_LAB.c_no = sts_po_qc.c_no 
-   and STS_QA_LAB.h_no = sts_po_qc.h_no 
-   and STS_QA_LAB.thick = sts_po_qc.thick 
-   and STS_QA_LAB.width = sts_po_qc.width
-where mv_bc_tag.active = 1 and matltran_mst.wc like '%FM%' $wh $date ";
-       
+   and Ltrim(rtrim(STS_QA_LAB.h_no)) = Ltrim(rtrim(sts_po_qc.h_no))
+   and Ltrim(rtrim(STS_QA_LAB.thick)) = Ltrim(rtrim(sts_po_qc.thick)) 
+   and Ltrim(rtrim(STS_QA_LAB.width)) = Ltrim(rtrim(sts_po_qc.width))
+where mv_bc_tag.active = 1 and matltran_mst.wc like '%FM%' $wh $date";
         $cSql = new SqlSrv();
         $rs0 = $cSql->SqlQuery($this->StrConn, $query);
         array_splice($rs0, count($rs0) - 1, 1);
