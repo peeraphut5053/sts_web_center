ca1c25f62eaad1d1bf1ee74de82cb061d5772b95
Pongteedee <pongteedee@gmail.com>
2024-09-27 15:50:00 +0700

    Update Data Test Lab and STS_Custom

--- module/RPT_STS_CUSTOM_MAINRPT_ACCT/data.php
+++ module/RPT_STS_CUSTOM_MAINRPT_ACCT/data.php
@@ -14,3 +14,13 @@ if ($load == "ajax") {
     $rs = $STS_Custom->GetSTS_custom_mainrpt_acct($StartDate, $EndDate);
     echo json_encode($rs);
 }
+
+if ($load == "reportPDF") {
+    $CallModel = new CallModel();
+    $CallModel->SyteLine_Models();
+
+    $STS_Custom = new STS_Custom();
+    $STS_Custom->setConn($ConnSL);
+    $rs = $STS_Custom->GetDataReportRemainPDF($StartDate, $EndDate);
+    echo json_encode($rs);
+}
