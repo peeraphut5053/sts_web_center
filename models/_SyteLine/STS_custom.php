7f7b9e145374d134810d11e0728572cf0bd1c870
Pongteedee <pongteedee@gmail.com>
2024-12-06 15:20:08 +0700

    Update Query STS_custom

--- models/_SyteLine/STS_custom.php
+++ models/_SyteLine/STS_custom.php
@@ -21,7 +21,7 @@ class STS_Custom {
         $rs0 = $cSql->SqlQuery($this->StrConn, $sql);
 
         if ($doc_no == $rs0[1]["doc_no"]) {
-            $query = "UPDATE STS_custom_IN set date_stock = '$date_stock', supplier = '$supplier', country = '$country', AD_rate = $AD_rate, weight_KG = $weight_KG, value = $value, remark = '$remark', item = '$item', type = $type where doc_no = '$doc_no'";
+            $query = "UPDATE STS_custom_IN set country = '$country', remark = '$remark', item = '$item', type = $type, createdate = GETDATE() where doc_no = '$doc_no'";
             $cSql = new SqlSrv();
             $rs0 = $cSql->SqlQuery($this->StrConn, $query);
             array_splice($rs0, count($rs0) - 1, 1);
@@ -44,26 +44,15 @@ class STS_Custom {
 
         if ($doc_no == $rs0[1]["doc_no"]) {
             $query = "UPDATE STS_custom_OUT set 
-    boatnote = '$boatnote', 
-    date = '$date',
-    date_submit = '$date_submit',
-    item = '$item', 
     boat_name = '$boat_name', 
     boat_no = '$boat_no', 
-    inv_no = '$inv_no',
-    bundle = $bundle, 
-    weight_net = $weight_net, 
-    weight_gross = $weight_gross, 
-    weight_zinc = " . ($weight_zinc !== '' ? $weight_zinc : 0.00) . ", 
-    weight_non_zinc = " . ($weight_nonzinc !== '' ? $weight_nonzinc : 0.00) . ", 
-    cust_po = '$cust_po', 
-    value = $value, 
     pier = '$pier', 
     BL_no = '$BL_no', 
     loc_name = '$loc_name',
     loc_name2 = '$loc_name2',
     loc_name3 = '$loc_name3',
     loc_name4 = '$loc_name4' 
+    createdate = GETDATE()
     where doc_no = '$doc_no'";
             $cSql = new SqlSrv();
             $rs0 = $cSql->SqlQuery($this->StrConn, $query);
@@ -85,7 +74,7 @@ class STS_Custom {
         $cSql = new SqlSrv();
         $rs0 = $cSql->SqlQuery($this->StrConn, $sql);
         if ($doc_no == $rs0[1]["doc_no"]) {
-            $query = "UPDATE STS_custom_scrap set date = '$date', item = '$item', weight_KG = $weight_KG, value = $value, stamp_no = '$stamp_no', ref_doc_no = '$ref_doc_no' where doc_no = '$doc_no'";
+            $query = "UPDATE STS_custom_scrap set item = '$item', stamp_no = '$stamp_no', ref_doc_no = '$ref_doc_no', createdate = GETDATE() where doc_no = '$doc_no'";
             $cSql = new SqlSrv();
             $rs0 = $cSql->SqlQuery($this->StrConn, $query);
             array_splice($rs0, count($rs0) - 1, 1);
