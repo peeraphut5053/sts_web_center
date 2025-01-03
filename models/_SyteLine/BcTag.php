402bbad94df3f1a221a732af644d7cc0b7cdb47d
Pongteedee <pongteedee@gmail.com>
2025-01-03 11:12:28 +0700

    Edit Api

--- models/_SyteLine/BcTag.php
+++ models/_SyteLine/BcTag.php
@@ -567,7 +567,7 @@ ORDER BY m.MonthNum, w.wcGroup";
 
     function getGroupChart($StartDate, $EndDate, $GroupBy) {
         $query = "select *
-from V_STS_PROD_machine_stat
+from V_STS_PROD_TIME_REPORT
 where [date] between '$StartDate' and '$EndDate'
   and wcGroup =  '$GroupBy'
 order by [date]";
