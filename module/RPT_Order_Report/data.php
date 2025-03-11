2d3d5c08423b559ebeb7c856ed15281bd38bd2d2
Pongteedee <pongteedee@gmail.com>
2025-03-11 15:25:06 +0700

    Update Order Report 2.0

--- module/RPT_Order_Report/data.php
+++ module/RPT_Order_Report/data.php
@@ -15,15 +15,15 @@ $Data = new JobOrder();
 $Data->setConn($ConnSL);
 
 if ($load == "Plated") {
-    $Data = $Data->PlatedOrderReport();
+    $Data = $Data->PlatedOrderReport($item, $wc);
     echo json_encode($Data);
 } else if ($load == "Packing") {
     $Data = $Data->PackingOrderReport($item,$wc);
     echo json_encode($Data);
 } else if ($load == "Production") {
-    $Data = $Data->ProductionOrderReport();
+    $Data = $Data->ProductionOrderReport($item,$wc);
     echo json_encode($Data);
 } else if ($load == "Hot") {
-    $Data = $Data->HotRollReport();
+    $Data = $Data->HotRollReport($item,$wc);
     echo json_encode($Data);
 }
