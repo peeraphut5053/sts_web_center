c792051f1ff09ff1f79a7e82119fcd1e2da5d7c9
Pongteedee <pongteedee@gmail.com>
2025-09-05 09:29:32 +0700

    Update Report Outstandingex

--- module/RPTINV_OUTSTANDINGEX/data.php
+++ module/RPTINV_OUTSTANDINGEX/data.php
@@ -17,17 +17,7 @@ if ($load == "form") {
     $CallModel->SyteLine_Models();
     $InvItem = new Invoice();
     $InvItem->setConn($ConnSL);
-    $InvItem->_txtFromDate_start = $txtFromDate_start;
-    $InvItem->_txtFromDate_end = $txtFromDate_end;
-    $InvItem->_txtFromCoNum_start = $txtFromCoNum_start;
-    $InvItem->_txtFromCoNum_end = $txtFromCoNum_end;
-    $InvItem->_txtFromCusnumOrName = $txtFromCusnumOrName;
-    $InvItem->_txtFromItemOrDes = $txtFromItemOrDes;
-    $InvItem->_txtCity = $txtCity;
-    $InvItem->_txtcustomerpo = $txtcustomerpo;
-    
-    $InvItem->_cus_type = 'EX';
-    $InvItems = $InvItem->GetRPTINV_OutstandingEX();
+    $InvItems = $InvItem->GetRPTINV_OutstandingEX($fromDate, $toDate, $cust_po, $sts_po);
     $InvItem = null;
     $CallModel = null;
     echo json_encode($InvItems);
