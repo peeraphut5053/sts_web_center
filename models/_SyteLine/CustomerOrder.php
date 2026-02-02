cb8e3cbc529df9d9f3f5f89c8cee675e3da1260e
Pongteedee <pongteedee@gmail.com>
2026-02-02 08:05:25 +0700

    Update Query Report item

--- models/_SyteLine/CustomerOrder.php
+++ models/_SyteLine/CustomerOrder.php
@@ -19,13 +19,7 @@ class CustomerOrder {
     }
 
     function CheckQty($item) {
-        $query = "SELECT distinct lot.loc, loc.description, lot.lot, lot.qty_on_hand, tag.uf_act_weight ,tag=tag.id
-FROM lot_loc_mst lot
-inner join location_mst loc on loc.loc = lot.loc
-left join mv_bc_tag tag on lot.item=tag.item and lot.lot=tag.lot and tag.qty1 = lot.qty_on_hand
-   and tag.active = 1 and tag.ship_stat <> 1
-Where lot.qty_on_hand > 0 
-and lot.item = '$item'";
+        $query = "EXEC [dbo].[STS_WebApp_salesIN_itemRpt] @item = '$item'";
         $cSql = new SqlSrv();
         $rs0 = $cSql->SqlQuery($this->StrConn, $query);
         array_splice($rs0, count($rs0) - 1, 1);
