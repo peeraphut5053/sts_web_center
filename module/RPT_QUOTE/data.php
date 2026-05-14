6ae521acf877d023409159ff1844410272092835
Pongteedee <pongteedee@gmail.com>
2026-05-14 09:07:44 +0700

    API

--- module/RPT_QUOTE/data.php
+++ module/RPT_QUOTE/data.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 
 foreach ($_GET as $key => $value) {
     $$key = trim($value);
--- module/Report_Repair/data.php
+++ module/Report_Repair/data.php
@@ -1,5 +1,5 @@
 <?php
-
+header("Access-Control-Allow-Origin: *");
 foreach ($_GET as $key => $value) {
     $$key = trim($value);
 }
