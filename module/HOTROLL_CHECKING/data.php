2b16263f0aa9f27461ee77b132def438945c9401
Pongteedee <pongteedee@gmail.com>
2026-09-15 16:18:31 +0700

    HOTROLL_CHECKING

--- module/HOTROLL_CHECKING/data.php
+++ module/HOTROLL_CHECKING/data.php
@@ -14,23 +14,63 @@ if ($load == 'ajax') {
     $po_QC->setConn($ConnSL);
     $where = "where 1=1";
     if (($txtFromDate != "") && ($txtToDate != "")) {
-        $where = $where . " AND ( qc.po_date BETWEEN '$txtFromDate' AND '$txtToDate' )  ";
+        $where .= " AND ( CONVERT(date, qc.po_date) BETWEEN '$txtFromDate' AND '$txtToDate' ) ";
+    } else if ($txtFromDate != "") {
+        $where .= " AND CONVERT(date, qc.po_date) >= '$txtFromDate' ";
+    } else if ($txtToDate != "") {
+        $where .= " AND CONVERT(date, qc.po_date) <= '$txtToDate' ";
     }
+
     if (($from_stsno != "") && ($to_stsno != "")) {
-        $where = $where . " AND ( qc.sts_no BETWEEN '$from_stsno' AND '$to_stsno' ) ";
+        if (is_numeric($from_stsno) && is_numeric($to_stsno)) {
+            $from_num = intval($from_stsno);
+            $to_num = intval($to_stsno);
+            $where .= " AND (
+                (TRY_CAST(qc.sno AS BIGINT) BETWEEN $from_num AND $to_num)
+                OR (TRY_CAST(qc.sts_no AS BIGINT) BETWEEN $from_num AND $to_num)
+                OR (qc.sno BETWEEN '$from_stsno' AND '$to_stsno')
+                OR (qc.sts_no BETWEEN '$from_stsno' AND '$to_stsno')
+            ) ";
+        } else {
+            $where .= " AND (
+                (qc.sno BETWEEN '$from_stsno' AND '$to_stsno')
+                OR (qc.sts_no BETWEEN '$from_stsno' AND '$to_stsno')
+            ) ";
+        }
     } else if ($from_stsno != "") {
-        $where = $where . " AND qc.sts_no >= '$from_stsno' ";
+        if (is_numeric($from_stsno)) {
+            $from_num = intval($from_stsno);
+            $where .= " AND (
+                (TRY_CAST(qc.sno AS BIGINT) >= $from_num)
+                OR (TRY_CAST(qc.sts_no AS BIGINT) >= $from_num)
+                OR (qc.sno >= '$from_stsno')
+                OR (qc.sts_no >= '$from_stsno')
+            ) ";
+        } else {
+            $where .= " AND (qc.sno >= '$from_stsno' OR qc.sts_no >= '$from_stsno') ";
+        }
     } else if ($to_stsno != "") {
-        $where = $where . " AND qc.sts_no <= '$to_stsno' ";
+        if (is_numeric($to_stsno)) {
+            $to_num = intval($to_stsno);
+            $where .= " AND (
+                (TRY_CAST(qc.sno AS BIGINT) <= $to_num)
+                OR (TRY_CAST(qc.sts_no AS BIGINT) <= $to_num)
+                OR (qc.sno <= '$to_stsno')
+                OR (qc.sts_no <= '$to_stsno')
+            ) ";
+        } else {
+            $where .= " AND (qc.sno <= '$to_stsno' OR qc.sts_no <= '$to_stsno') ";
+        }
     }
+
     if ($sno != "") {
-        $where = $where . " AND qc.sno like '%" . $sno . "%' ";
+        $where .= " AND (qc.sno like '%" . $sno . "%' OR qc.sts_no like '%" . $sno . "%') ";
     }
     if ($c_no != "") {
-        $where = $where . " AND qc.c_no like '%" . $c_no . "%' ";
+        $where .= " AND qc.c_no like '%" . $c_no . "%' ";
     }
     if ($h_no != "") {
-        $where = $where . " AND qc.h_no = '" . $h_no . "' ";
+        $where .= " AND qc.h_no like '%" . $h_no . "%' ";
     }
     $po_QC = $po_QC->GetRowsWithCond2($where);
     $CallModel = null;
